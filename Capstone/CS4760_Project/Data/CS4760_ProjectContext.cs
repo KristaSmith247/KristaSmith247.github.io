2959ef7498253477eac6c287ee01f0d90e803cb9
KristaSmith247 <118084776+KristaSmith247@users.noreply.github.com>
2025-01-06 13:24:54 -0700

    Added data, enums, migrations

---
+++ Capstone/CS4760_Project/Data/CS4760_ProjectContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CS4760_Project.Models;
+
+namespace CS4760_Project.Data
+{
+    public class CS4760_ProjectContext : DbContext
+    {
+        public CS4760_ProjectContext (DbContextOptions<CS4760_ProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CS4760_Project.Models.User> User { get; set; } = default!;
+        public DbSet<CS4760_Project.Models.Department> Department { get; set; } = default!;
+        public DbSet<CS4760_Project.Models.College> College { get; set; } = default!;
+        public DbSet<CS4760_Project.Models.Committee> Committee { get; set; } = default!;
+        public DbSet<CS4760_Project.Models.Grant> Grant { get; set; } = default!;
+        public DbSet<CS4760_Project.Models.FinancialRequest> FinancialRequest { get; set; } = default!;
+        public DbSet<CS4760_Project.Models.Review> Review { get; set; } = default!;
+        public DbSet<CS4760_Project.Models.RspgRubric> RspgRubric { get; set; } = default!;
+        public DbSet<CS4760_Project.Models.GrantCriteria> GrantCriteria { get; set; } = default!;
+        public DbSet<CS4760_Project.Models.RubricCriteria> RubricCriteria { get; set; } = default!;
+        public DbSet<CS4760_Project.Models.Report> Report { get; set; } = default!;
+    }
+}
---
+++ Capstone/CS4760_Project/Enums/FacultyRole.cs
@@ -0,0 +1,13 @@
+﻿namespace CS4760_Project.Enums
+{
+    public enum FacultyRole
+    {
+        Adjunct,
+        Tenured,
+        TenureTrack,
+        Instructor,
+        Staff,
+        Chair,
+        Dean
+    }
+}
